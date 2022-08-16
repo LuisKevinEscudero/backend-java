1082401e353815d045763b34bbd956b08cfb66c8
LuisKevinEscudero <110898636+LuisKevinEscudero@users.noreply.github.com>
2022-08-16 12:35:53 +0200

    ejercicio basico spring

---
+++ demo/src/main/java/com/bosonit/demo/Persona.java
@@ -0,0 +1,32 @@
+package com.bosonit.demo;
+
+
+public class Persona {
+    String nombre;
+    int edad;
+    String comentario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+}
