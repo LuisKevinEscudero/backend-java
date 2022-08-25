9754cecb82d306ddc5fda1b22f5d15232c704431
kevin <luiskevin.escudero@bosonit.com>
2022-08-25 12:58:53 +0200

    puesto dos tipos de logback.xml uno que no crea nada y guarda los mensaje de error y otro que crea un archivo para cada tipo de mensaje

--- ej5.5-logging/src/main/java/com/example/ej5/logging/controlador1.java
+++ ej5.5-logging/src/main/java/com/example/ej5/logging/controlador1.java
@@ -1,11 +1,13 @@
 package com.example.ej5.logging;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class controlador1 {
 
     Logger logger = LoggerFactory.getLogger(controlador1.class);
