8addf7be8215c51bfb56e88e61453d8b8cd6024c
kevin <luiskevin.escudero@bosonit.com>
2022-08-30 13:34:56 +0200

    intento de ejer 6.4 properties mal echo

---
+++ ej6.4-profiles/src/main/java/com/example/ej6/profiles/config.java
@@ -0,0 +1,8 @@
+package com.example.ej6.profiles;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class config {
+
+}
---
+++ ej6.4-profiles/src/main/java/com/example/ej6/profiles/controller1.java
@@ -0,0 +1,29 @@
+package com.example.ej6.profiles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller1 {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${server.url}")
+    private String url;
+
+    @Value("${server.password}")
+    private String password;
+
+    @GetMapping("parametros")
+    public String parametros() {
+        return "El valor de port es: "+port+" El valor de url es: "+url+" El valor de password es: "+password;
+    }
+}
+
