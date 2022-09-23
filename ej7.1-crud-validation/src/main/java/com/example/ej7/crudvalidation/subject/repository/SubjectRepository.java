52cccdc9a172988f27344e83f37cd6b720a031d9
kevin <luiskevin.escudero@bosonit.com>
2022-09-23 10:35:08 +0200

    cambio de nombre

--- ej7.1-crud-validation/src/main/java/com/example/ej7/crudvalidation/subject/repository/SubjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ej7.crudvalidation.subject.repository;
-
-import com.example.ej7.crudvalidation.subject.model.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, String> {
-
-}
--- ej7.1-crud-validation/src/main/java/com/example/ej7/crudvalidation/teacher/repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ej7.crudvalidation.teacher.repository;
-
-import com.example.ej7.crudvalidation.teacher.model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, String> {
-}
-
